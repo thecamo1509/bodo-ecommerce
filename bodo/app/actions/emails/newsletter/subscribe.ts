7d918fdd0022df8a2d71e4bf98b573f3588ccb12
Camilo <camilo@Camilos-MacBook-Pro.local>
2025-09-06 20:55:59 -0500

    feat: adding validation to env

--- bodo/app/actions/emails/newsletter/subscribe.ts
+++ bodo/app/actions/emails/newsletter/subscribe.ts
@@ -1,7 +1,10 @@
 "use server"
 
 export const subscribe = async (email: string) => {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || process.env.VERCEL_URL || 'http://localhost:3000'
+    // Detectar la URL base correctamente
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || process.env.NODE_ENV === 'production' ? 'https://bodo.com.co' : 
+                   'http://localhost:3000'
+    
     const response = await fetch(`${baseUrl}/api/emails/newsletter`, {
         method: "POST",
         headers: {
